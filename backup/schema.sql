3b626c39c8bc9ff2bd228a6040cec5399fb05c4f
Raweeroj Anontachutikarn <105781803+Raweej@users.noreply.github.com>
2023-11-17 15:48:45 +0700

    Update schema.sql

--- backup/schema.sql
+++ backup/schema.sql
@@ -20,13 +20,3 @@ CREATE TABLE Users (
     LastName VARCHAR(50),
     Email VARCHAR(100)
 );
-
-CREATE TABLE Checkouts (
-    CheckoutID INT PRIMARY KEY,
-    BookID INT,
-    UserID INT,
-    CheckoutDate DATE,
-    ReturnDate DATE,
-    FOREIGN KEY (BookID) REFERENCES Books(BookID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
-);
